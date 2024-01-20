ef4aadc29c0e625a30fe4adf871581de12a26132
Pratham Kakkad <prathamkakkad18@gmail.com>
2024-01-20 14:49:27 +0530

    functions structure added

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 class Calculator:
-
     def __init__(self):
         "this is the init function"
         pass
+    
+    def add(self, a, b):
+        "this is the add function"
+        pass
+
+    def subtract(self, a, b):
+        "this is the subtract function"
+        pass
+
+    def divide(self, a, b):
+        "this is the divide function"
+        pass
+
+    def multiply(self, a, b):
+        "this is the multiply function"
+        pass
+
+    def __str__(self):
+        "welcome to the calculator"
